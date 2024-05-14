e5318825929d4b88327749921c4aa391e928c7a0
GabbyMcCall998 <103126216+GabbyMcCall998@users.noreply.github.com>
2024-05-14 18:59:32 +0000

    Styled the Navigation bar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,30 @@
 
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css'; // Import CSS file for styling
 
 const Navbar = () => {
     return (  
-        <div>
-            <h1>God is the Greatest</h1>
+        <div className='Navbar'>
+            <div className='brand'>
+                <p>PaperTrail</p>
+            </div>
+            <ul className='nav-menu'>
+                <li>Home</li>
+                <li>Files</li>
+                <div className='search-container'>
+                <form  className='search' action="/search" method="get">
+                    <div className="search-wrapper">
+                    <input  type="text" placeholder="Search..." name="search" />
+                    <button type="submit">Search</button>
+                    </div>
+                </form>
+            </div>
+                <li>Profile</li>
+            </ul>
+            <div className='login-button'>
+                <button>Login</button>
+            </div>
         </div>
     );
 }
- 
 export default Navbar;
