cf7ae6afa68814ba0fc3edc60c888e5b1664ca3b
GabbyMcCall998 <103126216+GabbyMcCall998@users.noreply.github.com>
2024-05-18 21:44:14 +0000

    modified navigation bar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 
 import React, { useState } from 'react';
 import './Navbar.css'; // Import CSS file for styling
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
-    const [menu,setMenu] = useState("Home");
+    const [menu,setMenu] = useState("Home"); 
 
 
 
@@ -14,8 +15,8 @@ const Navbar = () => {
                 <p>PaperTrail</p>
             </div>
             <ul className='nav-menu'>
-                <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("file")}}>Files{menu==="file"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("home")}}><Link to='/' style={{textDecoration:'none'}} >Home</Link>{menu==="home"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("document")}}><Link to='/document' style={{textDecoration:'none'}} >Document</Link>{menu==="document"?<hr/>:<></>}</li>
                 <div className='search-container'>
                 <form  className='search' action="/search" method="get">
                     <div className="search-wrapper">
@@ -26,7 +27,7 @@ const Navbar = () => {
             </div>
             </ul>
             <div className='login-button'>
-                <button>Login</button>
+                <Link to='/login' ><button>Login</button></Link>
             </div>
         </div>
     );
